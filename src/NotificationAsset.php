379771822ad1e4248816361ff789330b40593a8d
Tarun M <tarun.mukherjee@indusnet.co.in>
2016-08-17 19:38:56 +0530

    modified asset bundle

--- src/NotificationAsset.php
+++ src/NotificationAsset.php
@@ -4,28 +4,27 @@ namespace tmukherjee13\notification;
 use yii\web\AssetBundle;
 
 /**
- * Main backend application asset bundle.
+ * Notification asset bundle.
  */
 class NotificationAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/tmukherjee13/yii2-live-notification/src/assets';
     public $css = [
     ];
     public $js = [
         'js/autobahn.min.js',
-        'js/notifier.js'
+        'js/notifier.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
 
-
     /**
      * @inheritdoc
      */
     public function init()
     {
+        $this->sourcePath = __DIR__ . '/assets';
         parent::init();
     }
 }
